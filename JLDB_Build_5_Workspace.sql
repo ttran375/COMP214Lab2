de2683889bee1a962a1200a22a18f2bca873db51
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-22 11:39:13 +0000

    Add solutions

--- JLDB_Build_5_Workspace.sql
+++ JLDB_Build_5_Workspace.sql
@@ -44,12 +44,32 @@ FROM
 WHERE
     TABLE_NAME = 'HOMEWORK13';
 
--- 5. Create a view that lists the ISBN and title for each book in inventory along with the name and phone number of the person to contact if the book needs to be reordered. Name the view REORDERINFO.
--- 6. Try to change the name of a contact person in the REORDERINFO view to your name. Was an error message displayed when performing this step? If so, what was the cause of the error message?
--- 7. Select one of the books in the REORDERINFO view and try to change its ISBN. Was an error message displayed when performing this step? If so, what was the cause of the error message?
--- 8. Delete the record in the REORDERINFO view containing your name. (If you weren’t able to perform #6 successfully, delete one of the contacts already listed in the table.) Was an error message displayed when performing this step? If so, what was the cause of the error message?
--- 9. Issue a rollback command to undo any changes made with the preceding DML operations.
--- 10. Delete the REORDERINFO view.
+-- 5. Create REORDERINFO view
+CREATE VIEW REORDERINFO AS
+SELECT b.ISBN, b.Title, p.Name AS Contact_Name, p.Phone AS Contact_Phone
+FROM Books b
+JOIN Publisher p ON b.PubID = p.PubID;
+
+-- 6. Try to change the name of a contact person
+-- This may or may not work depending on the view's updatable nature and your table structure.
+-- If an error occurs, it could be due to non-updatable view or lack of update privileges.
+UPDATE REORDERINFO SET Contact_Name = 'Your Name' WHERE ISBN = 'YourISBN';
+
+-- 7. Try to change the ISBN of a book
+-- Similar to #6, this depends on the updatable nature of the view and your table structure.
+-- If an error occurs, it could be due to non-updatable view or lack of update privileges.
+UPDATE REORDERINFO SET ISBN = 'NewISBN' WHERE ISBN = 'OldISBN';
+
+-- 8. Delete a record from REORDERINFO
+-- If there is an error, it could be due to non-updatable view or lack of delete privileges.
+DELETE FROM REORDERINFO WHERE Contact_Name = 'Your Name';
+
+-- 9. Rollback changes
+ROLLBACK;
+
+-- 10. Delete the REORDERINFO view
+DROP VIEW REORDERINFO;
+
 
 -- # Lab Exercises Module 2 - Database Objects
 
@@ -61,18 +81,66 @@ WHERE
 
 -- To perform the following assignments, refer to the tables in the JustLee Books database.
 
--- 1. Create a sequence for populating the Customer# column of the CUSTOMERS table. When setting the start and increment values, keep in mind that data already exists in this table. The options should be set to not cycle the values and not cache any values, and no minimum or maximum values should be declared.
--- 2. Add a new customer row by using the sequence created in Question 1. The only data currently available for the customer is as follows:
--- 3. last name = Shoulders, first name = Frank, and zip = 23567.
--- 4. Create a sequence that generates integers starting with the value 5. Each value should be three less than the previous value generated. The lowest possible value should be 0, and the sequence shouldn’t be allowed to cycle. Name the sequence MY_FIRST_SEQ.
--- 5. Issue a SELECT statement that displays NEXTVAL for MY_FIRST_SEQ three times. Because the value isn’t being placed in a table, use the DUAL table in the FROM clause of the SELECT statement. What causes the error on the third SELECT?
--- 6. Change the setting of MY_FIRST_SEQ so that the minimum value that can be generated is -1000.
--- 7. Create a private synonym that enables you to reference the MY_FIRST_SEQ object as NUMGEN.
--- 8. Use a SELECT statement to view the CURRVAL of NUMGEN. Delete the NUMGEN synonym and MY_FIRST_SEQ.
--- 9. Create a bitmap index on the CUSTOMERS table to speed up queries that search for customers based on their state of residence. Verify that the index exists, and then delete the index.
--- 10. Create a B-tree index on the customer’s Lastname column. Verify that the index exists by querying the data dictionary. Remove the index from the database.
--- 11. Many queries search by the number of days to ship (number of days between the order and shipping dates). Create an index that might improve the performance of these queries.
+-- 1. Create a sequence for populating the Customer# column of the CUSTOMERS table.
+CREATE SEQUENCE customer_seq
+  START WITH 1
+  INCREMENT BY 1
+  NOMAXVALUE
+  NOCYCLE
+  NOCACHE;
 
+-- 2. Add a new customer row using the sequence.
+INSERT INTO CUSTOMERS (Customer#, LastName, FirstName, Zip)
+VALUES (customer_seq.NEXTVAL, 'Shoulders', 'Frank', '23567');
+
+-- 4. Create a sequence MY_FIRST_SEQ starting from 5, decreasing by 3, with a minimum of 0.
+CREATE SEQUENCE my_first_seq
+  START WITH 5
+  INCREMENT BY -3
+  MINVALUE 0
+  NOCYCLE
+  NOCACHE;
+
+-- 5. Issue SELECT statement displaying NEXTVAL for MY_FIRST_SEQ three times.
+-- The error on the third SELECT is caused by reaching the MINVALUE (0) and going below it.
+SELECT my_first_seq.NEXTVAL FROM DUAL;
+SELECT my_first_seq.NEXTVAL FROM DUAL;
+SELECT my_first_seq.NEXTVAL FROM DUAL; -- Causes an error
+
+-- 6. Change the setting of MY_FIRST_SEQ so that the minimum value is -1000.
+ALTER SEQUENCE my_first_seq MINVALUE -1000;
+
+-- 7. Create a private synonym NUMGEN for MY_FIRST_SEQ.
+CREATE SYNONYM numgen FOR my_first_seq;
+
+-- 8. Use a SELECT statement to view the CURRVAL of NUMGEN.
+SELECT numgen.CURRVAL FROM DUAL;
+
+-- 8 (continued). Delete the NUMGEN synonym and MY_FIRST_SEQ.
+DROP SYNONYM numgen;
+DROP SEQUENCE my_first_seq;
+
+-- 9. Create a bitmap index on the CUSTOMERS table for the state column.
+CREATE BITMAP INDEX idx_customers_state ON CUSTOMERS(State);
+
+-- Verify that the index exists.
+SELECT index_name FROM user_indexes WHERE table_name = 'CUSTOMERS' AND index_name = 'IDX_CUSTOMERS_STATE';
+
+-- Delete the index.
+DROP INDEX idx_customers_state;
+
+-- 10. Create a B-tree index on the customer’s Lastname column.
+CREATE INDEX idx_customers_lastname ON CUSTOMERS(LastName);
+
+-- Verify that the index exists.
+SELECT index_name FROM user_indexes WHERE table_name = 'CUSTOMERS' AND index_name = 'IDX_CUSTOMERS_LASTNAME';
+
+-- Remove the index.
+DROP INDEX idx_customers_lastname;
+
+-- 11. Create an index for the number of days to ship.
+-- Assuming you have an ORDERS table with OrderDate and ShipDate columns.
+CREATE INDEX idx_days_to_ship ON ORDERS(ShipDate - OrderDate);
 
 -- **Advanced Challenge**
 
@@ -80,8 +148,34 @@ WHERE
 
 -- # Case Study
 
--- The head DBA has requested the creation of a sequence for the primary key columns of the Criminals and Crimes tables. After creating the sequences, add a new criminal named Johnny Capps to the Criminals table by using the correct sequence. (Use any values for the remainder of columns.)
--- A crime needs to be added for the criminal, too. Add a row to the Crimes table, referencing the sequence value already generated for the Criminal_ID and using the correct sequence to generate the Crime_ID value. (Use any values for the remainder of columns).
--- The last name, street, and phone number columns of the Criminals table are used quite often in the WHERE clause condition of queries. Create objects that might improve data retrieval for these queries.
--- Would a bitmap index be appropriate for any columns in the City Jail database (assuming the columns are used in search and/or sort operations)? If so, identify the columns and explain why a bitmap index is appropriate for them.
--- Would using the City Jail database be any easier with the creation of synonyms? Explain why or why not.
+-- 1. Create sequences for the primary key columns of the Criminals and Crimes tables.
+CREATE SEQUENCE criminals_seq START WITH 1 INCREMENT BY 1 NOCYCLE NOCACHE;
+CREATE SEQUENCE crimes_seq START WITH 1 INCREMENT BY 1 NOCYCLE NOCACHE;
+
+-- 2. Add a new criminal named Johnny Capps to the Criminals table using the sequence.
+INSERT INTO Criminals (Criminal_ID, Last_Name, First_Name, Street, Phone)
+VALUES (criminals_seq.NEXTVAL, 'Capps', 'Johnny', '123 Main St', '555-1234');
+
+-- 3. Add a crime for the criminal to the Crimes table using the generated sequence values.
+INSERT INTO Crimes (Crime_ID, Criminal_ID, Crime_Type, Crime_Date)
+VALUES (crimes_seq.NEXTVAL, criminals_seq.CURRVAL, 'Robbery', SYSDATE);
+
+-- 4. Create indexes for columns frequently used in WHERE clause conditions.
+CREATE INDEX idx_criminals_last_name ON Criminals(Last_Name);
+CREATE INDEX idx_criminals_street ON Criminals(Street);
+CREATE INDEX idx_criminals_phone ON Criminals(Phone);
+
+-- 5. Determine if a bitmap index is appropriate for any columns in the City Jail database.
+-- Bitmap indexes are suitable for low cardinality columns, like gender or crime type.
+-- Assuming Crime_Type is low cardinality, you might create a bitmap index:
+CREATE BITMAP INDEX idx_crimes_crime_type ON Crimes(Crime_Type);
+
+-- 6. Determine if synonyms would make using the City Jail database easier.
+-- Synonyms can provide abstraction and simplify queries if table or view names change.
+-- If there are frequent changes to table/view names, synonyms might be helpful.
+-- Example of creating synonyms:
+CREATE SYNONYM cj_criminals FOR Criminals;
+CREATE SYNONYM cj_crimes FOR Crimes;
+-- Queries can then use cj_criminals and cj_crimes instead of Criminals and Crimes.
+
+-- Note: Adjust column names and table names based on your actual database schema.

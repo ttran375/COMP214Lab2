b07802322ff2bb94fbb4938277d000e1af313f39
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-29 18:32:34 +0000

    Mon, Jan 29, 2024, 6:32 PM +00:00

--- Case_Study_06.sql
+++ Case_Study_06.sql
@@ -1,11 +1,37 @@
 -- 6. An INSERT statement is needed to support users adding a new appeal. Create an INSERT statement using substitution variables. Note that users will be entering dates in the format of a two-digit month, a two-digit day, and a four-digit year, such as “12 17 2009.” In addition, a sequence named APPEALS_ID_SEQ exists to supply values for the Appeal_ID column, and the default setting for the Status column should take effect (that is, the DEFAULT option on the column should be used). Test the statement by adding the following appeal: crime_ID ¼ 25344031, filing date ¼ 02 13 2009, and hearing date ¼ 02 27 2009.
+SET SERVEROUTPUT ON
+
+-- Prompt the user for input with default values
+ACCEPT crime_id CHAR DEFAULT 25344031 PROMPT 'Enter Crime_ID [Default: 25344031]: '
+
+ACCEPT filing_date CHAR DEFAULT '02 13 2009' PROMPT 'Enter Filing_Date (MM DD YYYY) [Default: 02 13 2009]: '
+
+ACCEPT hearing_date CHAR DEFAULT '02 27 2009' PROMPT 'Enter Hearing_Date (MM DD YYYY) [Default: 02 27 2009]: '
+
+-- Declare variables to store user input
 DECLARE
-  v_crime_id NUMBER := 25344031;
-  v_filing_date VARCHAR2(10) := '02 13 2009';
-  v_hearing_date VARCHAR2(10) := '02 27 2009';
+  V_CRIME_ID     NUMBER := &CRIME_ID;
+  V_FILING_DATE  DATE := TO_DATE('&filing_date', 'MM DD YYYY');
+  V_HEARING_DATE DATE := TO_DATE('&hearing_date', 'MM DD YYYY');
 BEGIN
-  INSERT INTO Appeals (Appeal_ID, Crime_ID, Filing_Date, Hearing_Date, Status)
-  VALUES (APPEALS_ID_SEQ.NEXTVAL, v_crime_id, TO_DATE(v_filing_date, 'MM DD YYYY'), TO_DATE(v_hearing_date, 'MM DD YYYY'), DEFAULT);
-  COMMIT;
+ -- Insert statement using substitution variables
+  INSERT INTO APPEALS (
+    APPEAL_ID,
+    CRIME_ID,
+    FILING_DATE,
+    HEARING_DATE
+  ) VALUES (
+    APPEALS_ID_SEQ.NEXTVAL,
+    V_CRIME_ID,
+    V_FILING_DATE,
+    V_HEARING_DATE
+  );
+ -- Display a success message
+  DBMS_OUTPUT.PUT_LINE('Appeal successfully added.');
+EXCEPTION
+  WHEN OTHERS THEN
+ -- Display an error message
+    DBMS_OUTPUT.PUT_LINE('Error: '
+                         || SQLERRM);
 END;
 /

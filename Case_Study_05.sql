0fb357de450a2df71df53c19f1cc7cd2bdf47fc2
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-29 18:25:07 +0000

    Mon, Jan 29, 2024, 6:25 PM +00:00

--- Case_Study_05.sql
+++ Case_Study_05.sql
@@ -1,11 +1,11 @@
 -- 5. Display the criminal name and probation start date for all criminals who have a probation period greater than two months. Also, display the date that’s two months from the beginning of the probation period, which will serve as a review date.
-select
-  c.last || ', ' || c.first as criminal_name,
-  s.start_date as probation_start_date,
-  add_months(s.start_date, 2) as review_date
-from
+SELECT
+  c.last || ', ' || c.first AS criminal_name,
+  s.start_date AS probation_start_date,
+  ADD_MONTHS(s.start_date, 2) AS review_date
+FROM
   criminals_dw c
-  join sentences s on c.criminal_id = s.criminal_id
-where
+  JOIN sentences s ON c.criminal_id = s.criminal_id
+WHERE
   s.type = 'P'
-  and round(months_between(s.end_date, s.start_date)) > 2;
+  AND MONTHS_BETWEEN(s.end_date, s.start_date) > 2;

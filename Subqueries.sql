0534ebab1b3d086f2b612c16c7b2b96e84ab39d7
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-29 20:13:05 +0000

    Mon, Jan 29, 2024, 8:13 PM +00:00

--- Subqueries.sql
+++ Subqueries.sql
@@ -1,45 +1,95 @@
 -- 1. List the book title and retail price for all books with a retail price lower than the average retail price of all books sold by JustLee Books.
-SELECT title, retail
-FROM books
-WHERE retail >
-(SELECT AVG(retail)
-FROM books);
+SELECT
+  title,
+  retail
+FROM
+  books
+WHERE
+  retail < (
+    SELECT
+      AVG(retail)
+    FROM
+      books
+  );
 
 -- 2. Determine which books cost less than the average cost of other books in the same category.
-SELECT a.title, b.category, a.cost
-FROM books a, (SELECT category, AVG(cost) averagecost
-FROM books
-GROUP BY category) b
-WHERE a.category = b.category
-AND a.cost < b.averagecost;
+SELECT
+  a.title,
+  b.category,
+  a.cost
+FROM
+  books a,
+  (
+    SELECT
+      category,
+      AVG(cost) averagecost
+    FROM
+      books
+    GROUP BY
+      category
+  ) b
+WHERE
+  a.category = b.category
+  AND a.cost < b.averagecost;
 
 -- 3. Determine which orders were shipped to the same state as order 1014.
-SELECT order#
-FROM orders
-WHERE shipstate = (SELECT shipstate
-FROM orders
-WHERE order# = 1014);
+SELECT
+  order#
+FROM
+  orders
+WHERE
+  shipstate = (
+    SELECT
+      shipstate
+    FROM
+      orders
+    WHERE
+      order# = 1014
+  );
 
 -- 4. Determine which orders had a higher total amount due than order 1008.
-SELECT order#, SUM(quantity * paideach)
-FROM orderitems
-GROUP BY order#
-HAVING SUM(quantity * paideach) >
-(SELECT SUM(quantity * paideach)
-FROM orderitems
-WHERE order# = 1008);
+SELECT
+  order#,
+  SUM(quantity * paideach)
+FROM
+  orderitems
+GROUP BY
+  order#
+HAVING
+  SUM(quantity * paideach) > (
+    SELECT
+      SUM(quantity * paideach)
+    FROM
+      orderitems
+    WHERE
+      order# = 1008
+  );
 
 -- 5. Determine which author or authors wrote the books most frequently purchased by customers of JustLee Books.
-SELECT lname, fname
-FROM bookauthor JOIN author USING(authorid)
-WHERE isbn IN
-(SELECT isbn
-FROM orderitems
-GROUP BY isbn
-HAVING SUM(quantity) =
-(SELECT MAX(COUNT(*))
-FROM orderitems
-GROUP BY isbn));
+SELECT
+  lname,
+  fname
+FROM
+  bookauthor
+  JOIN author USING (authorid)
+WHERE
+  isbn IN (
+    SELECT
+      isbn
+    FROM
+      orderitems
+    GROUP BY
+      isbn
+    HAVING
+      SUM(quantity) = (
+        SELECT
+          MAX(COUNT(*))
+        FROM
+          orderitems
+        GROUP BY
+          isbn
+      )
+  );
 
 -- 6. List the title of all books in the same category as books previously purchased by customer 1007. Don’t include books this customer has already purchased.
 SELECT DISTINCT
@@ -79,18 +129,34 @@ ORDER BY
   B.TITLE;
 
 -- 7. List the shipping city and state for the order that had the longest shipping delay.
-SELECT shipcity, shipstate
-FROM orders
-WHERE shipdate-orderdate =
-(SELECT MAX(shipdate-orderdate)
-FROM orders);
+SELECT
+  shipcity,
+  shipstate
+FROM
+  orders
+WHERE
+  shipdate - orderdate = (
+    SELECT
+      MAX(shipdate - orderdate)
+    FROM
+      orders
+  );
 
 -- 8. Determine which customers placed orders for the least expensive book (in terms of regular retail price) carried by JustLee Books.
-SELECT customer#
-FROM customers JOIN orders USING(customer#) JOIN orderitems USING(order#) JOIN books USING(isbn)
-WHERE retail =
-(SELECT MIN(retail)
-FROM books);
+SELECT
+  customer#
+FROM
+  customers
+  JOIN orders USING (customer#)
+  JOIN orderitems USING (order#)
+  JOIN books USING (isbn)
+WHERE
+  retail = (
+    SELECT
+      MIN(retail)
+    FROM
+      books
+  );
 
 -- 9. Determine the number of different customers who have placed an order for books written or cowritten by James Austin.
 SELECT
@@ -106,10 +172,16 @@ WHERE
   AND A.FNAME = 'JAMES';
 
 -- 10. Determine which books were published by the publisher of The Wok Way to Cook.
-SELECT title
-FROM books
-WHERE pubid =
-(SELECT pubid
-FROM books
-WHERE title = 'THE WOK WAY TO COOK');
-
+SELECT
+  title
+FROM
+  books
+WHERE
+  pubid = (
+    SELECT
+      pubid
+    FROM
+      books
+    WHERE
+      title = 'THE WOK WAY TO COOK'
+  );

dfebd1195fabb7e3f95db4d505e375ed232994d2
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-29 10:23:12 +0000

    Mon, Jan 29, 2024, 10:23 AM +00:00

--- Advanced_Challenge_Workspace.sql
+++ Advanced_Challenge_Workspace.sql
@@ -8,21 +8,21 @@
 -- Price, and Revised Price. Sort the results by category. If there’s more
 -- than one book in a category, a secondary sort should be performed on the
 -- book’s title.
-
 -- Create a document to show management the SELECT statement used to
 -- generate the results and the results of the statement.
-
 SELECT
   Title,
   Category,
   TO_CHAR(Retail, '$9999.99') AS "Current Price",
-  TO_CHAR(Retail * 
-    CASE
+  TO_CHAR(
+    Retail * CASE
       WHEN Category = 'Computer' THEN 1.10
       WHEN Category = 'Fitness' THEN 1.15
       WHEN Category = 'Self Help' THEN 1.25
       ELSE 1.03
-    END, '$9999.99') AS "Revised Price"
+    END,
+    '$9999.99'
+  ) AS "Revised Price"
 FROM
   Books
 ORDER BY

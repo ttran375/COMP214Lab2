a261bba34973c86113d4d3cc693c30bb1593c6bb
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-05 14:22:40 +0000

    Mon, Feb 5, 2024, 2:22 PM +00:00

--- Subqueries_01.sql
+++ Subqueries_01.sql
@@ -1,13 +1,13 @@
 -- 1. List the book title and retail price for all books with a retail price lower than the average retail price of all books sold by JustLee Books.
-SELECT
-  title,
-  retail
-FROM
-  books
-WHERE
-  retail < (
-    SELECT
-      AVG(retail)
-    FROM
-      books
+Select
+  TITLE,
+  RETAIL
+From
+  BOOKS
+Where
+  RETAIL < (
+    Select
+      Avg(RETAIL)
+    From
+      BOOKS
   );

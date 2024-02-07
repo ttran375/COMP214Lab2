3aae0d5ad0f7bf8b40267738e1feb70e6567cc39
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-07 13:10:11 +0000

    Wed, Feb 7, 2024, 1:10 PM +00:00

--- Subqueries.sql
+++ Subqueries.sql
@@ -30,17 +30,11 @@ WHERE
     AND A.Cost < B.Averagecost;
 
 -- 3. Determine which orders were shipped to the same state as order 1014.
-SELECT
-    Order#
-FROM
-    Orders
-WHERE
-    Shipstate = (
-        SELECT
-            Shipstate
-        FROM
-            Orders
-        WHERE
-            Order# = 1014
-    )
-    AND Order# <> 1014;
+SELECT Order#
+FROM Orders
+WHERE Shipstate = (
+    SELECT Shipstate
+    FROM Orders
+    WHERE Order# = 1014
+)
+AND Order# <> 1014;

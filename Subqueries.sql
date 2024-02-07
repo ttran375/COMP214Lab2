5244fd40ac2c740d7890184845163f0758965eb2
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-07 13:45:30 +0000

    Wed, Feb 7, 2024, 1:45 PM +00:00

--- Subqueries.sql
+++ Subqueries.sql
@@ -30,11 +30,120 @@ WHERE
     AND A.Cost < B.Averagecost;
 
 -- 3. Determine which orders were shipped to the same state as order 1014.
-SELECT Order#
+SELECT "Order#"
 FROM Orders
-WHERE Shipstate = (
-    SELECT Shipstate
-    FROM Orders
-    WHERE Order# = 1014
-)
-AND Order# <> 1014;
+WHERE
+    Shipstate = (
+        SELECT Shipstate
+        FROM Orders
+        WHERE "Order#" = 1014
+    )
+    AND "Order#" != 1014;
+
+-- 4. Determine which orders had a higher total amount due than order 1008.
+SELECT
+    "Order#",
+    SUM(Quantity * Paideach) AS Totalamountdue
+FROM
+    Orderitems
+GROUP BY
+    "Order#"
+HAVING
+    SUM(Quantity * Paideach) > (
+        SELECT SUM(Quantity * Paideach)
+        FROM
+            Orderitems
+        WHERE
+            "Order#" = 1008
+    );
+
+-- 5. Determine which author or authors wrote the books most frequently 
+-- purchased by customers of JustLee Books.
+SELECT
+    Author.Authorid,
+    Author.Lname,
+    Author.Fname,
+    (
+        SELECT COUNT(*)
+        FROM
+            Orderitems
+        WHERE
+            Orderitems.Isbn IN (
+                SELECT Bookauthor.Isbn
+                FROM
+                    Bookauthor
+                WHERE
+                    Bookauthor.Authorid = Author.Authorid
+            )
+    ) AS Purchasecount
+FROM
+    Author
+ORDER BY
+    Purchasecount DESC
+FETCH FIRST 1 ROW ONLY;
+
+-- 6. List the title of all books in the same category as books previously 
+-- purchased by customer 1007. Don’t include books this customer has already 
+-- purchased.
+SELECT DISTINCT Books.Title
+FROM
+    Orders,
+    Orderitems,
+    Books
+WHERE
+    Orders."Order#" = Orderitems."Order#"
+    AND Orderitems.Isbn = Books.Isbn
+    AND Books.Category IN (
+        SELECT DISTINCT Books.Category
+        FROM
+            Orders,
+            Orderitems,
+            Books
+        WHERE
+            Orders."Order#" = Orderitems."Order#"
+            AND Orderitems.Isbn = Books.Isbn
+            AND Orders."Customer#" = 1007
+    )
+    AND Books.Isbn NOT IN (
+        SELECT Isbn
+        FROM
+            Orderitems
+        WHERE
+            "Order#" IN (
+                SELECT "Order#"
+                FROM
+                    Orders
+                WHERE
+                    "Customer#" = 1007
+            )
+    );
+
+-- 7. List the shipping city and state for the order that had the longest 
+-- shipping delay.
+SELECT
+    "Order#",
+    Shipcity,
+    Shipstate,
+    Shipdate - Orderdate AS Shippingdelay
+FROM
+    Orders
+WHERE
+    Shipdate IS NOT NULL
+ORDER BY
+    Shippingdelay DESC
+FETCH FIRST 1 ROWS ONLY;
+
+-- 8. Determine which customers placed orders for the least expensive book (in 
+-- terms of regular retail price) carried by JustLee Books.
+SELECT c."Customer#", c.LastName, c.FirstName
+FROM Customers c
+INNER JOIN Orders o ON c."Customer#" = o."Customer#"
+INNER JOIN OrderItems oi ON o."Order#" = oi."Order#"
+WHERE oi.ISBN = (
+    SELECT ISBN
+    FROM Books
+    WHERE Retail = (
+        SELECT MIN(Retail)
+        FROM Books
+    )
+);
